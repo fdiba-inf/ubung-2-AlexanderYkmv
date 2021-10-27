17c33d878512f60d273af4bf60fa20e1431b89d6
AlexanderYkmv <sasho.yakimov@gmail.com>
2021-10-27 17:50:34 +0300

    три

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -1,4 +1,48 @@
 package exercise2;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        //System.out.println("Enter a,b,c: ");
+        //System.out.print("a:");
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        //System.out.print("b:");
+        double b = input.nextDouble();
+        //System.out.print("c:");
+        double c = input.nextDouble();
+
+        if (a!=0){
+            double D = b*b - 4*a*c;
+            if (D>=0){
+                double x1 = (-b + Math.sqrt(D))/2*a;
+                double x2 = (-b - Math.sqrt(D))/2*a;
+                System.out.println(x1 +", "+ x2);
+                //System.out.println(x2);
+            } else {
+                System.out.println("Imaginary values");
+            }
+        }else {
+            if (b!=0){
+                double x3 = -c/b;
+                System.out.println(x3);
+            } else {
+                if (c!=0){
+                    System.out.println("No values");
+                }else {
+                    System.out.println("Many values");
+                }
+            }
+
+        }
+
+
+
+    }
+}
+/* package exercise2;
  import java.lang.Math;
 
 import java.util.Scanner;
@@ -39,4 +83,4 @@ public class QuadraticEquation {
 
 
     }
-}
+} */
